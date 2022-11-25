c58b7538c4dc0df6e282c4e8cf871925900dca06
Haniel Ubogu <hanieltobi@gmail.com>
2022-11-25 14:56:48 +0100

    minor clean

--- packages/config/vite.config.ts
+++ packages/config/vite.config.ts
@@ -1,28 +1,9 @@
 import type { UserConfig } from 'vite';
 import unocss from 'unocss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { join } from 'path';
-import { readFileSync } from 'fs';
-import { cwd } from 'process';
 
-const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json')));
 const config: UserConfig = {
-  plugins: [unocss(), sveltekit()],
-  build: {
-    target: ['es2020'],
-    sourcemap: true
-  },
-  resolve: {
-    preserveSymlinks: false
-  },
-  ssr: {
-    noExternal: Object.keys(pkg.dependencies || {})
-  },
-  server: {
-    open: '/'
-  }
+  plugins: [unocss(), sveltekit()]
 };
-
-if (process.env.NODE_ENV === 'production') config.resolve!.preserveSymlinks = true;
 
 export default config;
